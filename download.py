331da99421a276d87c4a81af696973e458609a76
James Hines <jameshines10@gmail.com>
2023-10-03 21:36:47 +0000

    Initial commit

---
+++ download.py
@@ -0,0 +1,65 @@
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.oauth2 import service_account
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaIoBaseDownload
+import io
+
+def get_service(api_name, api_version, scopes, key_file_location):
+    """Get a service that communicates to a Google API.
+
+    Args:
+        api_name: The name of the api to connect to.
+        api_version: The api version to connect to.
+        scopes: A list auth scopes to authorize for the application.
+        key_file_location: The path to a valid service account JSON key file.
+
+    Returns:
+        A service that is connected to the specified API.
+    """
+
+    credentials = service_account.Credentials.from_service_account_file(
+    key_file_location)
+
+    scoped_credentials = credentials.with_scopes(scopes)
+
+    # Build the service object.
+    service = build(api_name, api_version, credentials=scoped_credentials)
+
+    return service
+
+def main():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+
+    # Define the auth scopes to request.
+    scope = 'https://www.googleapis.com/auth/drive.file'
+    key_file_location = 'service-account.json'
+
+    try:
+        # Authenticate and construct service.
+        service = get_service(
+            api_name='drive',
+            api_version='v3',
+            scopes=[scope],
+            key_file_location=key_file_location)
+
+        # Call the Drive v3 API
+        request_file = service.files().get_media(fileId="1IflBBDLHCdxaD9tgywI3NFo_AV-O0K9i")
+        file = io.BytesIO()
+        downloader = MediaIoBaseDownload(file, request_file)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print(F'Download {int(status.progress() * 100)}.')
+        file_retrieved: str = file.getvalue()
+        with open(f"RPSContract.json", 'wb') as f:
+            f.write(file_retrieved)
+    except HttpError as error:
+        # TODO(developer) - Handle errors from drive API.
+        print(f'An error occurred: {error}')
+
+if __name__ == '__main__':
+    main()
---
+++ upload.py
@@ -0,0 +1,57 @@
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.oauth2 import service_account
+from googleapiclient.http import MediaFileUpload
+
+def get_service(api_name, api_version, scopes, key_file_location):
+    """Get a service that communicates to a Google API.
+
+    Args:
+        api_name: The name of the api to connect to.
+        api_version: The api version to connect to.
+        scopes: A list auth scopes to authorize for the application.
+        key_file_location: The path to a valid service account JSON key file.
+
+    Returns:
+        A service that is connected to the specified API.
+    """
+
+    credentials = service_account.Credentials.from_service_account_file(
+    key_file_location)
+
+    scoped_credentials = credentials.with_scopes(scopes)
+
+    # Build the service object.
+    service = build(api_name, api_version, credentials=scoped_credentials)
+
+    return service
+
+def main():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+
+    # Define the auth scopes to request.
+    scope = 'https://www.googleapis.com/auth/drive.file'
+    key_file_location = 'service-account.json'
+
+    try:
+        # Authenticate and construct service.
+        service = get_service(
+            api_name='drive',
+            api_version='v3',
+            scopes=[scope],
+            key_file_location=key_file_location)
+
+        # Call the Drive v3 API
+        file_metadata = {'name': 'RPSContract.json'}
+        media = MediaFileUpload('/root/rps-contracts/build/contracts/RPSContractFactory.json', mimetype='text/json')
+
+        file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    except HttpError as error:
+        # TODO(developer) - Handle errors from drive API.
+        print(f'An error occurred: {error}')
+
+if __name__ == '__main__':
+    main()
